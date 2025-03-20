1679519dc6dfc630e4e628ff6f5922bb81efeec1
Jasmine Sehgal <jasminesehgal690@gmail.com>
2025-03-20 17:27:01 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int a;
-    scanf("%d",&a);
-    for (int i = 1; i <= a; i++){
-        for (int j = 1; j < a; j++){
-             if (i == 1 || i == a || j == 1 || j == a) {
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        if (i == 1 || i == a) {
+            // Print full row of asterisks for the first and last row
+            for (int j = 1; j <= a; j++) {
                 printf("*");
-            } 
+            }
+        } else {
+            // Print hollow row for the middle row
+            printf("*"); // Print the first asterisk
+            for (int j = 2; j < a; j++) {
+                printf(" "); // Print spaces in between
+            }
+            printf("*"); // Print the last asterisk
         }
-        printf("\n");
+        printf("\n"); // Move to the next line after each row
     }
+
     return 0;
 }
